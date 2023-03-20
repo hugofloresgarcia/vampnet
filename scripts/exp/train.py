dde5c212d420bfaba0a7d4100fc02c5fc53e200c
Hugo Flores Garcia <hfgarcia@descript.com>
2023-03-20 17:43:31 +0000

    loudnorm,

--- scripts/exp/train.py
+++ scripts/exp/train.py
@@ -59,7 +59,7 @@ IGNORE_INDEX = -100
 @argbind.bind("train", "val", without_prefix=True)
 def build_transform():
     transform = transforms.Compose(
-        tfm.VolumeNorm(("uniform", -32, -14)),
+        tfm.VolumeNorm(("uniform", -32, -20)),
         tfm.VolumeChange(("uniform", -6, 3)),
         tfm.RescaleAudio(),
     )
