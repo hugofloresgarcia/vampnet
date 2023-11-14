34652c75d5848bc061e1965a6d648b25ced9f328
Orr Paradise <orrp@berkeley.edu>
2023-11-14 15:05:49 -0800

    Added plotly requirement for gtzan_embeddings.py

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "loralib",
         "torch_pitch_shift",
         "madmom",
-        "pyharp @ git+https://github.com/audacitorch/pyharp.git"
+        "pyharp @ git+https://github.com/audacitorch/pyharp.git",
+        "plotly",
     ],
 )
